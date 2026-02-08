b0f2c4537b5fb3dae41b54c27f7d28f7aa5aa4c5
Ira Horecka <ira89@icloud.com>
2026-02-07 20:34:31 -0500

    add test for default plot

--- tests/test_plot_smoke.py
+++ tests/test_plot_smoke.py
@@ -26,3 +26,32 @@ def test_plotter_smoke(toy_results):
         Plotter(toy_results).plot_matrix().plot_cluster_labels().show()
     finally:
         plt.show = plt_show
+
+
+@pytest.mark.api
+def test_plotter_stacked_defaults_smoke(toy_results):
+    """
+    Ensures a stacked default Plotter chain renders without explicit style kwargs.
+
+    Args:
+        toy_results (Results): Results fixture for plotting.
+    """
+    matplotlib = pytest.importorskip("matplotlib")
+    matplotlib.use("Agg", force=True)
+    # Suppress GUI rendering during tests
+    plt_show = plt.show
+    plt.show = lambda *args, **kwargs: None
+    try:
+        (
+            Plotter(toy_results)
+            .plot_dendrogram()
+            .plot_matrix()
+            .plot_matrix_axis_labels()
+            .plot_row_ticks()
+            .plot_col_ticks()
+            .plot_cluster_labels()
+            .plot_cluster_bar(name="sigbar")
+            .show()
+        )
+    finally:
+        plt.show = plt_show
